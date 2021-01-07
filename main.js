5f9d5784859b92c1ec0fea8765a08eb991d57312
Ruairi Conway <hi@ruairiconway.com>
2021-01-07 13:13:13 -0500

    added basic quote functionality

--- main.js
+++ main.js
@@ -1,5 +1,47 @@
 'use strict'
 
+
+// ==== QUOTE ROTATION
+
+const quotes = [
+    {
+        line: `"Only the present moment is real."`,
+        attr: `- Thich Nhat Hanh`
+    },
+    {
+        line: `"The road to recovery is the road to life."`,
+        attr: `- Bessel Van der Kolk`
+    },
+    {
+        line: `"Who looks outside dreams, who looks inside awakes."`,
+        attr: `- Carl Yung`
+    }
+]
+
+let count = 0
+
+function handleQuote(quotes) {
+    let limit = quotes.length - 1
+    if (count !== limit) {
+        count += 1
+        return quotes[count]
+    } else {
+        count = 0
+        return quotes[count]
+    }
+}
+
+function cycleQuote() {
+    setInterval(() => {
+        let currentQuote = handleQuote(quotes)
+        $(`.js-quote-line`).html(`${currentQuote.line}`)
+        $(`.js-quote-attr`).html(`${currentQuote.attr}`)
+    }, 3000);
+}
+
+
+// ==== CONTENT ACCORDIAN
+
 function psychoButton() {
     $('#content-psycho > .content-button').on("click", () => {
         $('#content-psycho > .content-p').toggleClass('hidden', 1000, "easeOutSine");
@@ -24,4 +66,5 @@ function watchContentButtons() {
     approachButton()
 }
 
+$(cycleQuote)
 $(watchContentButtons)
